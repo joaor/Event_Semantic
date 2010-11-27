e29c8af3cf80c8057073d87223c704db1bed9a05
João Rodrigues <joao.marques.rodrigues@gmail.com>
2010-11-27 18:30:54 +0000

    RDF Population done. Bug related do CDATA nor resolved

--- event_semantics/create_rdf.py
+++ event_semantics/create_rdf.py
@@ -1,13 +1,17 @@
 from datetime import datetime, timedelta
 from rdflib import Namespace, Literal, URIRef
 
-import settings, os
+import settings, os, unicodedata, re
 from django.core.management import setup_environ
 setup_environ(settings)
 from django_rdf import graph
 
 from events.semantic import ontologies
 from xml.dom.minidom import parseString
+
+
+def strip_accents(s):
+   return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
 
 def getText(nodelist):
     rc = []
@@ -17,10 +21,18 @@ def getText(nodelist):
     return ''.join(rc)
 
 place_list = []
-folder = '/Users/joaorodrigues/Documents/5_ano/ws/Projecto/Projecto_WS/Data/events/'
+artist_list = []
+events_folder = '/Users/joaorodrigues/Documents/5_ano/ws/Projecto/Projecto_WS/Data/events/'
+artists_folder = '/Users/joaorodrigues/Documents/5_ano/ws/Projecto/Projecto_WS/Data/artists/'
+albums_folder = '/Users/joaorodrigues/Documents/5_ano/ws/Projecto/Projecto_WS/Data/albums/'
+repeat = False
+a = 0
+b= 0
+album_number = 1
+cdata = "<!\[CDATA\[(.*?<a.*?a>\.)*"
 
-for filename in os.listdir(folder)[1:]:
-	f = open("%s/%s" % (folder,filename), 'r')
+for filename in os.listdir(events_folder)[1:]:
+	f = open("%s/%s" % (events_folder,filename), 'r')
 	rdf = ''
 	for line in f:
 		rdf +=line
@@ -54,25 +66,91 @@ for filename in os.listdir(folder)[1:]:
 					graph.add((place,ontologies['me']['Long'],Literal(float(lng))))
 				else:
 					graph.add((event,ontologies['me']['takes_place'],place))
-					break
+					repeat = True
 			else:		
-				address = getText(description.getElementsByTagName("v:street-address")[0].childNodes)
-				graph.add((place,ontologies['me']['Address'],Literal(address)))
-				locality = getText(description.getElementsByTagName("v:locality")[0].childNodes)
-				graph.add((place,ontologies['me']['Locality'],Literal(locality)))
-				country = getText(description.getElementsByTagName("v:country-name")[0].childNodes)
-				graph.add((place,ontologies['me']['Country'],Literal(country)))
-				postal = getText(description.getElementsByTagName("v:postal-code")[0].childNodes)
-				graph.add((place,ontologies['me']['Postal'],Literal(postal)))
-				graph.add((event,ontologies['me']['takes_place'],place))
-				break
+				if not repeat:
+					address = getText(description.getElementsByTagName("v:street-address")[0].childNodes)
+					graph.add((place,ontologies['me']['Address'],Literal(address)))
+					locality = getText(description.getElementsByTagName("v:locality")[0].childNodes)
+					graph.add((place,ontologies['me']['Locality'],Literal(locality)))
+					country = getText(description.getElementsByTagName("v:country-name")[0].childNodes)
+					graph.add((place,ontologies['me']['Country'],Literal(country)))
+					postal = getText(description.getElementsByTagName("v:postal-code")[0].childNodes)
+					graph.add((place,ontologies['me']['Postal'],Literal(postal)))
+					graph.add((event,ontologies['me']['takes_place'],place))
+				else:
+					repeat = False
+		elif description.getAttribute("rdf:about")[:33] == "http://lastfm.rdfize.com/artists/":
+			try:
+				artist_id = str(strip_accents(getText(description.getElementsByTagName("rdfs:label")[0].childNodes)).replace(' ','+'))
+				artist = ontologies['me'][artist_id]
+				artist_list.append(artist)
+				graph.add((artist,ontologies['rdf']['type'],ontologies['me']['Performer']))
+				graph.add((event,ontologies['me']['performed_by'],artist))
+			except:
+				pass
+			
+for filename in os.listdir(artists_folder)[1:]:
+	f = open("%s/%s" % (artists_folder,filename), 'r')
+	rdf = ''
+	for line in f:
+		rdf +=line
+	rdf = re.sub(cdata,'', rdf)
+	rdf = rdf.replace("]]>",'')
+	print filename
+	xml = parseString(rdf)
+	lfm = xml.getElementsByTagName("lfm")[0]
+	art = lfm.getElementsByTagName("artist")[0]
+	artist_name = getText(art.getElementsByTagName("name")[0].childNodes).replace('\"','\'')
+	artist_id = str(strip_accents(artist_name).replace(' ','+'))
+	artist = ontologies['me'][artist_id]
+	if artist in artist_list:
+		graph.add((artist,ontologies['me']['Name'],Literal(artist_name)))
+		tags = art.getElementsByTagName("tags")[0]
+		for tag in tags.getElementsByTagName("tag"):
+			artist_genre = getText(tag.getElementsByTagName("name")[0].childNodes)
+			graph.add((artist,ontologies['me']['Genre'],Literal(artist_genre)))
+		bio = art.getElementsByTagName("bio")[0]
+		summary = getText(bio.getElementsByTagName("summary")[0].childNodes)
+		print summary
+		#graph.add((artist,ontologies['me']['Summary'],Literal(summary)))
+		#description = getText(bio.getElementsByTagName("content")[0].childNodes)
+		#graph.add((artist,ontologies['me']['Description'],Literal(description)))
+		#print description
+
+for filename in os.listdir(albums_folder)[1:]:
+	f = open("%s/%s" % (albums_folder,filename), 'r')
+	jso = ''
+	for line in f:
+		jso +=line
+	info = eval(jso)
+	artist_id = str(info['Artist']['Name'])
+	artist = ontologies['me'][artist_id]
+	if artist in artist_list:
+		for alb in info['Artist']['Albums'].keys():
+			try:
+				album = ontologies['me'][str(album_number)] 
+				graph.add((album,ontologies['rdf']['type'],ontologies['me']['Album']))
+				album_name = str(alb)
+				graph.add((album,ontologies['me']['Name'],Literal(album_name)))
+				album_label = str(info['Artist']['Albums'][alb])
+				graph.add((album,ontologies['me']['Label'],Literal(album_label)))
+				graph.add((artist,ontologies['me']['recorded'],album))
+				album_number += 1
+			except:
+				continue
+		
+mylist = list(set(artist_list))
+print len(mylist)	
+print a
+print b
 				
 #graph.add((ontologies['me']['artist1'],ontologies['rdf']['type'],ontologies['me']['Artist']))
 #graph.add((ontologies['me']['artist1'],ontologies['me']['Description'],Literal('ola, sou o artista1')))
 
 #graph.add((ontologies['me']['event1'],ontologies['me']['performed_by'],ontologies['me']['artist1']))
 
-#from pprint import pprint
+from pprint import pprint
 #pprint(list(graph))
 
 
@@ -91,6 +169,6 @@ for filename in os.listdir(folder)[1:]:
 # who is what age?
 #pprint(list(graph.subject_objects(ontologies['me']['description'])))	
 
-print list(graph.query(""" SELECT ?cenas WHERE { ?cenas me:takes_place ?a . ?a me:Name "MusicBox" . } """, initNs=ontologies))
+#print list(graph.query(""" SELECT ?cenas WHERE { ?cenas me:takes_place ?a . ?a me:Name "MusicBox" . } """, initNs=ontologies))
 
 #graph.commit()
